a21a28669ca10e797c95ab0da9daf16bc39d5242
BarakpaevOlzhas <40425093+BarakpaevOlzhas@users.noreply.github.com>
2019-05-31 18:22:24 +0600

    Add files via upload

---
+++ NodeApp/NodeApp/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NodeApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        static UserText bufText = new UserText();        
+
+        Timer timer = new Timer(SaveText, bufText, TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(10));
+
+        
+
+        public MainWindow()
+        {
+            Timer timerLoad = new Timer(LoadText, null, TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(1));
+            bufText.TextBox = mainText;
+            InitializeComponent();            
+            mainText = bufText.TextBox;
+            timerLoad.Dispose();
+        }
+
+        ~MainWindow()
+        {
+            timer.Dispose();
+        }
+
+        private static void SaveText(object textBox)
+        {           
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fileStream = new FileStream("Buf.bit", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fileStream, bufText);
+            }
+        }
+
+        private void LoadText(object textBox)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            if (System.IO.File.Exists("Buf.bit"))
+                using (FileStream fileStream = new FileStream("Buf.bit", FileMode.OpenOrCreate))
+            {
+                bufText = (UserText)formatter.Deserialize(fileStream);
+                    mainText = bufText.TextBox;
+            }
+        }
+    }
+}
---
+++ ThreadPoolLesson/ThreadPoolLesson/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPoolLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0;i < 20;i++)
+            //{
+            //    ThreadPool.QueueUserWorkItem(PrintNumbers);
+            //}
+
+            Timer timer = new Timer(PrintTime, null, TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(1));
+
+            Console.ReadLine();
+            timer.Dispose();
+        }
+
+        static void PrintTime(object state )
+        {
+            var now = DateTime.Now;
+            var currentThread = Thread.CurrentThread;
+
+            Console.WriteLine($"Поток с Id {currentThread.ManagedThreadId} показывает время {now.ToString("hh:mm:ss")}");
+        }
+
+        static void PrintNumbers(object data)
+        {
+            var currentThread = Thread.CurrentThread;
+
+            Console.WriteLine($"Поток с Id {currentThread.ManagedThreadId} начал работу");
+
+            for (int i = 0;i < 10; i++)
+            {
+                Thread.Sleep(200);
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine($"Поток с Id закончил { currentThread.ManagedThreadId}работу");
+        }
+    }
+}
